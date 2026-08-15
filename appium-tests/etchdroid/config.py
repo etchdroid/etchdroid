71f9e0621f5de94f51ae40850c93250d757c7965
Davide Depau <davide@depau.eu>
2026-08-15 21:51:39 +0200

    fix(appium): unbound localhost host on macos

--- appium-tests/etchdroid/config.py
+++ appium-tests/etchdroid/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     ANDROID_HOME = os.environ.get("ANDROID_HOME", os.path.expanduser("~/Android/Sdk"))
-    APPIUM_HOST = os.environ.get("APPIUM_HOST", "127.0.0.10")  # Avoid collisions with manually started Appium servers
+    APPIUM_HOST = os.environ.get("APPIUM_HOST", "127.0.0.1")
     APPIUM_PORT = os.environ.get("APPIUM_PORT", "14723")
 
     QEMU_QMP_PATH = os.environ.get("QEMU_QMP_PATH", "/tmp/qmp.sock")
